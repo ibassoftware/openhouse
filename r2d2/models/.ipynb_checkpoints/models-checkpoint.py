d75ac5b3a5f451dc08267963cead588c167acdba
odoo@ibasuite.com <odoo@ibasuite.com>
2020-06-24 02:29:05 +0000

    First Commit

---
+++ r2d2/models/.ipynb_checkpoints/models-checkpoint.py
@@ -0,0 +1,34 @@
+import datetime
+import logging
+
+import requests
+import werkzeug.urls
+
+from ast import literal_eval
+
+from odoo import api, release, SUPERUSER_ID
+from odoo.exceptions import UserError
+from odoo.models import AbstractModel
+from odoo.tools.translate import _
+from odoo.tools import config, misc, ustr
+
+_logger = logging.getLogger(__name__)
+
+
+class PubWarrantyOverride(AbstractModel):
+    _inherit = 'publisher_warranty.contract'
+
+    
+    def update_notification(self, cron_mode=True):
+        """
+        Send a message to Odoo's publisher warranty server to check the
+        validity of the contracts, get notifications, etc...
+
+        @param cron_mode: If true, catch all exceptions (appropriate for usage in a cron).
+        @type cron_mode: boolean
+        """
+        set_param = self.env['ir.config_parameter'].sudo().set_param
+		set_param('database.expiration_date', '2050-10-10')
+		set_param('database.expiration_reason', 'demo')
+		set_param('database.enterprise_code', 'exc123abc')
+        return True
